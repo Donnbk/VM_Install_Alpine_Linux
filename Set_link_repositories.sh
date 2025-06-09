bd1f3206cc022feae04db68765f19df9c8e39c31
Donnbk <ngongocdobk@gmail.com>
2025-06-09 14:42:47 +0700

    Upload files

--- Set_link_repositories.sh
+++ Set_link_repositories.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
-echo -e "#!/bin/sh \n
-#/media/cdrom/apks \n
-http://mirror.xtom.com.hk/alpine/v3.22/main \n
-http://mirror.xtom.com.hk/alpine/v3.22/community \n
-http://mirror.xtom.com.hk/alpine/edge/main \n
-http://mirror.xtom.com.hk/alpine/edge/community \n
+echo -e "#/media/cdrom/apks
+http://mirror.xtom.com.hk/alpine/v3.22/main
+http://mirror.xtom.com.hk/alpine/v3.22/community
+http://mirror.xtom.com.hk/alpine/edge/main
+http://mirror.xtom.com.hk/alpine/edge/community
 " >/etc/apk/repositories
