629e0df799ad392fc28d91fe2fbc3f8123b7e0e1
Donnbk <ngongocdobk@gmail.com>
2025-06-10 05:14:17 +0700

    Update file

--- Set_link_repositories.sh
+++ Set_link_repositories.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
 echo -e "#/media/cdrom/apks
+#http://dl-cdn.alpinelinux.org/alpine/v3.22/main
+#http://dl-cdn.alpinelinux.org/alpine/v3.22/community
+#http://dl-cdn.alpinelinux.org/alpine/edge/main
+#http://dl-cdn.alpinelinux.org/alpine/edge/community
+
 http://mirror.xtom.com.hk/alpine/v3.22/main
 http://mirror.xtom.com.hk/alpine/v3.22/community
 http://mirror.xtom.com.hk/alpine/edge/main
